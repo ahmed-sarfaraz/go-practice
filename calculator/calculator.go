fb890d2b80204af5f9b2bd179653e0dda630f009
Ahmed Sarfaraz <ahmed.j.sarfaraz@gmail.com>
2026-07-12 18:29:06 +0530

    Refine calculator

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,42 +1,57 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 )
 
-func runCalculator(a, b, c int) int {
+func runOperation(a, b, c int) (int, error) {
 	switch c {
 	case 1:
-		return a + b
+		return a + b, nil
 	case 2:
-		return a - b
+		return a - b, nil
 	case 3:
-		return a * b
+		return a * b, nil
 	case 4:
 		if b == 0 {
-			fmt.Println("Divide by zero error.")
-			return 0
+			return 0, errors.New("Divide by zero error.")
 		}
-		return a / b
+		return a / b, nil
 	default:
-		fmt.Println("Invalid operation. Please try again.")
-		return 0
+		return 0, errors.New("Invalid operation. Please try again.")
 	}
-
 }
 
-func main() {
+func getOperation(op int) string {
+	switch op {
+	case 1:
+		return "+"
+	case 2:
+		return "-"
+	case 3:
+		return "*"
+	case 4:
+		return "/"
+	}
+	return ""
+}
+
+func Calculator() {
 	fmt.Println("Operator")
-	var a int
+	var a string
 	_, err := fmt.Scan(&a)
+	Operator, err := strconv.Atoi(a)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
 	fmt.Println("Operand")
-	var b int
+	var b string
 	_, err = fmt.Scan(&b)
+	Operand, err := strconv.Atoi(b)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
@@ -47,12 +62,24 @@ func main() {
 	fmt.Println("2. Subtraction")
 	fmt.Println("3. Multiplication")
 	fmt.Println("4. Division")
-	var c int
+	var c string
 	_, err = fmt.Scan(&c)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
+	Operation, err := strconv.Atoi(c)
+	res, err := runOperation(Operator, Operand, Operation)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	fmt.Print(runCalculator(a, b, c))
+	fmt.Printf("%d %s %d = %d\n", Operand, getOperation(Operation), Operator, res)
+
+}
+
+func main() {
+	Calculator()
+
 }
