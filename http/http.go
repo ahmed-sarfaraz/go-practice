3acb2a25d92f384e765fbafa7b148094bbd49446
Ahmed Sarfaraz <ahmed.j.sarfaraz@gmail.com>
2026-07-15 19:10:42 +0530

    use http standard library

--- http/http.go
+++ http/http.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 type Data struct {
@@ -14,23 +19,73 @@ type Data struct {
 	Completed  bool
 }
 
-func main() {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	data := string(body)
-	fmt.Println(string(body))
-	var test Data
-	json.Unmarshal([]byte(data), &test)
-	fmt.Printf("%v\n", test)
-	http.HandleFunc("/", fooHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func getMethod(url string) *http.Request {
+	ctx := context.TODO()
+	const method string = "GET"
+	var body io.Reader = nil
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return req
 }
 
-func fooHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func getMethodWithParameters() {
+	ctx := context.TODO()
+	const method string = "GET"
+	const path string = "https://scryfall.com/search"
+	var body io.Reader = nil
+	v := make(url.Values)
+	v.Add("q", `"of Emrakul"`)
+	v.Add("order", "released")
+	v.Add("direction", "asc")
+	dst := path + "?" + v.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, method, dst, body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Write(os.Stdout)
+}
+
+func writeResponseToLocalFile() {
+	log.SetPrefix("writeResponseToLocalFile: ")
+	dir := flag.String("dir", ".", "where the file will be created")
+	timeout := flag.Duration("timeout", 30*time.Second, "timeout for download")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) != 2 {
+		log.Fatal("usage: ./http [-timeout duration] url filename")
+	}
+
+	url, filename := args[0], args[1]
+	c := http.Client{Timeout: *timeout}
+	req := getMethod(url)
+	res, err := c.Do(req)
+	if err != nil {
+		log.Fatal(fmt.Errorf("request: %v", err))
+	}
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatal(fmt.Errorf("request with code: %d", res.StatusCode))
+	}
+	defer res.Body.Close()
+	dstPath := filepath.Join(*dir, filename)
+	dstFile, err := os.Create(dstPath)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Error creating file %v", err))
+	}
+	defer dstFile.Close()
+
+	if _, err = io.Copy(dstFile, res.Body); err != nil {
+		log.Fatal(fmt.Errorf("copying response to file: %v", err))
+	}
+}
+
+func main() {
+	writeResponseToLocalFile()
 }
